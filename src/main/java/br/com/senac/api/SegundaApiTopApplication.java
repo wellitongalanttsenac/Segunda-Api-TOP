3cbd13b50fc1427cb03629ce64e0e19676132d1a
WÉLLITON GALANT CAETANO <welliton.caetano@alunos.sc.senac.br>
2026-02-26 19:51:41 -0300

    Primeiro commit API TOP

---
+++ src/main/java/br/com/senac/api/SegundaApiTopApplication.java
@@ -0,0 +1,14 @@
+package br.com.senac.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaApiTopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SegundaApiTopApplication.class, args);
+	}
+
+}
